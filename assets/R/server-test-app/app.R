c57130e2cf5473afd887ff109bd44131f93ef9e2
DivadNojnarg <dgranjon@ymail.com>
2022-10-20 07:43:53 +0200

    add server test, output validation, profiling

---
+++ assets/R/server-test-app/app.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+ui <- fluidPage(
+  fluidPage(
+    sliderInput(
+      "obs",
+      "Number of observations:",
+      min = 0,
+      max = 1000,
+      value = 500
+    ),
+    plotOutput("distPlot")
+  )
+)
+
+server <- function(input, output, session) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs))
+  })
+}
+
+shinyApp(ui, server)
